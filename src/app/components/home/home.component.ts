b1561579130eed1d1629ba9ec4ea5013e137b560
progaurab <progaurab@gmail.com>
2021-06-15 17:50:56 +0530

    added class binding

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -19,4 +19,13 @@ export class HomeComponent implements OnInit {
     this.clickCounter += 1;
   }
 
+  setClasses() {
+    let myClasses = {
+      active: this.clickCounter > 4,
+      notactive: this.clickCounter <= 4
+    };
+
+    return myClasses;
+  }
+
 }

b60ca4597569da4a1b42be009ba554b4c4412cfe
progaurab <progaurab@gmail.com>
2021-06-15 15:22:34 +0530

    added routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+import { ListComponent } from './list/list.component'
+
+
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'list', component: ListComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
